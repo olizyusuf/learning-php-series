59656e1d8c0705c71cb2bcc838809b00037271e5
olizyusuf <olizyusuf22@gmail.com>
2024-01-14 13:14:56 +0700

    Studi Kasus oop selesai

--- 04. Studi Kasus OOP/App.php
+++ 04. Studi Kasus OOP/App.php
@@ -1,15 +1,19 @@
 <?php
 
-require_once __DIR__ . "/Model/TodoList.php";
-require_once __DIR__ . "/BusinessLogic/AddTodoList.php";
-require_once __DIR__ . "/BusinessLogic/RemoveTodoList.php";
-require_once __DIR__ . "/BusinessLogic/ShowTodoList.php";
-require_once __DIR__ . "/View/ViewShowTodoList.php";
-require_once __DIR__ . "/View/ViewAddTodoList.php";
-require_once __DIR__ . "/View/ViewRemoveTodoList.php";
+use Repository\TodolistRepositoryImpl;
+use Services\TodolistServiceImpl;
+use View\TodolistView;
+
+require_once __DIR__ . "/Entity/Todolist.php";
+require_once __DIR__ . "/Helper/InputHelper.php";
+require_once __DIR__ . "/Repository/TodolistRepository.php";
+require_once __DIR__ . "/Service/TodolistService.php";
+require_once __DIR__ . "/View/TodolistView.php";
 
 echo "Aplikasi Todolist" . PHP_EOL;
 
-viewShowTodoList();
+$todolistRepository = new TodolistRepositoryImpl();
+$todolistService = new TodolistServiceImpl($todolistRepository);
+$todolistView = new TodolistView($todolistService);
 
-echo __DIR__ . PHP_EOL;
+$todolistView->showTodolist();
--- 04. Studi Kasus OOP/View/TodolistView.php
+++ 04. Studi Kasus OOP/View/TodolistView.php
@@ -1,18 +1,73 @@
 <?php
 
 namespace View {
+
+    use Helper\InputHelper;
+    use Services\TodolistService;
+
     class TodolistView
     {
+        private TodolistService $todolistService;
+
+        public function __construct(TodolistService $todolistService)
+        {
+            $this->todolistService  = $todolistService;
+        }
+
         function showTodolist(): void
         {
+            while (true) {
+                $this->todolistService->showTodolist();
+
+                echo "" . PHP_EOL;
+                echo "=   Menu  =" . PHP_EOL;
+                echo "===========" . PHP_EOL;
+
+                echo "1. Tambah Todo" . PHP_EOL;
+                echo "2. Hapus Todo" . PHP_EOL;
+                echo "x. Keluar" . PHP_EOL;
+
+                $pilihan = InputHelper::input("Pilih");
+
+                if ($pilihan == "1") {
+                    $this->addTodolist();
+                } else if ($pilihan == "2") {
+                    $this->removeTodolist();
+                } else if ($pilihan == "x") {
+                    break;
+                } else {
+                    echo "pilihan menu tidak ditemukan" . PHP_EOL;
+                    echo "" . PHP_EOL;
+                }
+            }
+
+            echo "Bye bye...." . PHP_EOL;
         }
 
         function addTodolist(): void
         {
+            echo "Menambah TODO" . PHP_EOL;
+
+            $todo = InputHelper::input("Todo (x untuk batal)");
+
+            if ($todo != "x") {
+                $this->todolistService->addTodolist($todo);
+            } else {
+                echo "Batal tambah todo" . PHP_EOL;
+            }
         }
 
         function removeTodolist(): void
         {
+            echo "Menghapus Todo" . PHP_EOL;
+
+            $pilihan = InputHelper::input("Nomer (x untuk batal)");
+
+            if ($pilihan != "x") {
+                $this->todolistService->removeTodolist($pilihan);
+            } else {
+                echo "Batal menghapus todo" . PHP_EOL;
+            }
         }
     }
 }
